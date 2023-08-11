4294893e3e3f003f79b1ba9278454183c0996b49
Tinfoil-droid <137796947+Tinfoil-droid@users.noreply.github.com>
2023-08-11 14:49:48 +0100

    fixed display bug and text align

--- scripts.js
+++ scripts.js
@@ -1,63 +1,65 @@
-const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelector('.calculatorKeys')
-const display = calculator.querySelector('.calculatorDisplay')
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector(".calculatorKeys");
+const display = calculator.querySelector(".calculatorDisplay");
 
+keys.addEventListener("click", (event) => {
+  if (!event.target.closest("button")) return; //if a user clicks on the grid gap, it selects the calculatorKeys div, instead of a specific button. This line prevents this.
 
-keys.addEventListener('click', event => {
-    if (!event.target.closest('button')) return; //if a user clicks on the grid gap, it selects the calculatorKeys div, instead of a specific button. This line prevents this.
+  const key = event.target;
+  const keyValue = key.textContent;
+  const displayValue = display.textContent;
+  const type = key.dataset.type;
+  const previousKeyType = calculator.dataset.previousKeyType;
 
-    const key = event.target
-    const keyValue = key.textContent
-    const displayValue = display.textContent
-    const type = key.dataset.type
-    const previousKeyType = calculator.dataset.previousKeyType
-    
-    if (type === 'number') {
-        if (displayValue === '0') {
-            display.textContent = keyValue
-        } else if (previousKeyType === 'operator') 
-        {
-            display.textContent = keyValue
-        } else {
-            display.textContent = displayValue + keyValue
-        }
+  if (type === "number") {
+    if (displayValue === "0" || previousKeyType === "operator") {
+      display.textContent = keyValue;
+    } else {
+      display.textContent = displayValue + keyValue;
     }
-    
-    if (type === 'operator') {
-        const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
-        operatorKeys.forEach(el => { el.dataset.state = '' })
-        key.dataset.state = 'selected'
+  }
 
-        calculator.dataset.firstNumber = displayValue
-        calculator.dataset.operator = key.dataset.key
+  if (type === "operator") {
+    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+    operatorKeys.forEach((el) => {
+      el.dataset.state = "";
+    });
+    key.dataset.state = "selected";
+
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
+  }
+
+  if (type === "equal") {
+    const firstNumber = calculator.dataset.firstNumber;
+    const operator = calculator.dataset.operator;
+    const secondNumber = displayValue;
+    if (operator) {
+      display.textContent = calculate(firstNumber, operator, secondNumber);
     }
+  }
 
-    if (type === 'equal') {
-        const firstNumber = calculator.dataset.firstNumber
-        const operator = calculator.dataset.operator
-        const secondNumber = displayValue
-        display.textContent = calculate(firstNumber, operator, secondNumber)
-    }
+  if (type === "clear") {
+    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+    operatorKeys.forEach((el) => (el.dataset.state = ""));
+    key.dataset.state = "";
 
-    if (type === 'clear') {
-        const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
-        operatorKeys.forEach(el => el.dataset.state = '')
-        key.dataset.state = ''
+    calculator.dataset.firstNumber = "0";
+    display.textContent = 0;
+  }
 
-        calculator.dataset.firstNumber = '0'
-        display.textContent = 0
-    }
-
-    calculator.dataset.previousKeyType = type
+  calculator.dataset.previousKeyType = type;
 });
 
-function calculate (firstNumber, operator, secondNumber) {
-    firstNumber = parseInt(firstNumber)
-    secondNumber = parseInt(secondNumber)
-    let result = ''
-        if (operator === 'plus') result = firstNumber + secondNumber
-        if (operator === 'minus') result = firstNumber - secondNumber
-        if (operator === 'times') result = firstNumber * secondNumber
-        if (operator === 'divide') result = firstNumber / secondNumber
-        return result
+function calculate(firstNumber, operator, secondNumber) {
+  firstNumber = parseFloat(firstNumber);
+  secondNumber = parseFloat(secondNumber);
+  let result = "";
+
+  if (operator === "plus") result = firstNumber + secondNumber;
+  if (operator === "minus") result = firstNumber - secondNumber;
+  if (operator === "times") result = firstNumber * secondNumber;
+  if (operator === "divide") result = firstNumber / secondNumber;
+
+  return result;
 }
